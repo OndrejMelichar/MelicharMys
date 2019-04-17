37d518ca4b6d8fa46491afad1b8fff82cf0f384d
Ondřej Melichar <MelOnd@outlook.cz>
2019-04-17 12:26:51 +0200

    práce hnusná

--- MelicharMys/App.xaml.cs
+++ MelicharMys/App.xaml.cs
@@ -37,9 +37,9 @@ namespace MelicharMys
 
         private void CreateContextMenu()
         {
-            /*_notifyIcon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
+            _notifyIcon.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip();
             _notifyIcon.ContextMenuStrip.Items.Add("MainWindow...").Click += (s, e) => ShowMainWindow();
-            _notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, e) => ExitApplication();*/
+            _notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, e) => ExitApplication();
 
             _notifyIcon.Click += _notifyIcon_DoubleClick;
         }
