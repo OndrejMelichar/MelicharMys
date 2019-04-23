27e510e50343d5bc79e0dcf3955896082560b3fe
Ondřej Melichar <MelOnd@outlook.cz>
2019-04-23 09:00:34 +0200

    mění se rychlost myši

--- MelicharMys/ContextWindow.xaml.cs
+++ MelicharMys/ContextWindow.xaml.cs
@@ -23,9 +23,10 @@ namespace MelicharMys
         public ContextWindow()
         {
             InitializeComponent();
-        }
 
-        public const UInt32 SPI_GETMOUSESPEED = 0x0070;
+            MouseOptions.InternalSetMouseSpeed(30);
+            mouseSpeedValueTextBox.Text = MouseOptions.CurrentSpeed.ToString();
+        }
 
         [DllImport("User32.dll")]
         static extern Boolean SystemParametersInfo(
@@ -33,90 +34,8 @@ namespace MelicharMys
             UInt32 uiParam,
             IntPtr pvParam,
             UInt32 fWinIni);
-
-        static unsafe void InitializeMain()
-        {
-            MouseOptions m = new MouseOptions();
-
-            MouseOptions.GetDefaults();
-            int speed;
-            SystemParametersInfo(SPI_GETMOUSESPEED, 0, new IntPtr(&speed), 0);
-            Console.WriteLine(speed);
-
-            MouseOptions.SetDefaults();
-
-
-            SystemParametersInfo(SPI_GETMOUSESPEED, 0, new IntPtr(&speed), 0);
-            Console.WriteLine(speed);
-
-
-            Console.ReadLine();
-        }
-
-
-
-        public class MouseOptions
-        {
-            [DllImport("user32.dll")]
-            public static extern int SystemParametersInfo(int uAction, int uParam, IntPtr lpvParam, int fuWinIni);
-
-            [DllImport("kernel32.dll")]
-            public static extern int GetLastError();
-
-            public const int SPI_GETMOUSESPEED = 112;
-            public const int SPI_SETMOUSESPEED = 113;
-
-
-            private static int intDefaulSpeed = 10;
-            private static int intCurrentSpeed;
-            private static int intNewSpeed;
-
-            public static void GetDefaults()
-            {
-                intCurrentSpeed = GetMouseSpeed();
-            }
-            public static void SetDefaults()
-            {
-                if (intCurrentSpeed == 20)
-                {
-                    SetMouseSpeed(intDefaulSpeed);
-                }
-                else if (intCurrentSpeed < 10)
-                {
-                    SetMouseSpeed(intDefaulSpeed);
-                }
-            }
-
-            public static int GetMouseSpeed()
-            {
-                int intSpeed = 0;
-                IntPtr ptr;
-                ptr = Marshal.AllocCoTaskMem(4);
-                SystemParametersInfo(SPI_GETMOUSESPEED, 0, ptr, 0);
-                intSpeed = Marshal.ReadInt32(ptr);
-                Marshal.FreeCoTaskMem(ptr);
-
-                return intSpeed;
-            }
-
-            public static void SetMouseSpeed(int intSpeed)
-            {
-                IntPtr ptr = new IntPtr(intSpeed);
-
-                int b = SystemParametersInfo(SPI_SETMOUSESPEED, 0, ptr, 0);
-
-                if (b == 0)
-                {
-                    Console.WriteLine("Not able to set speed");
-                }
-                else if (b == 1)
-                {
-                    Console.WriteLine("Successfully done");
-                }
-
-            }
-
-        }
+        
+        
 
     }
 }
---
+++ MelicharMys/MouseOptions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MelicharMys
+{
+    public class MouseOptions
+    {
+        [DllImport("user32.dll")]
+        public static extern int SystemParametersInfo(int uAction, int uParam, IntPtr lpvParam, int fuWinIni);
+        [DllImport("kernel32.dll")]
+        public static extern int GetLastError();
+        private const int SPI_GETMOUSESPEED = 112;
+        private const int SPI_SETMOUSESPEED = 113;
+        public static int CurrentSpeed;
+        
+        public static void SetDefaults()
+        {
+            InternalSetMouseSpeed(5); // bylo zde 10
+        }
+
+        public static int InternalGetMouseSpeed()
+        {
+            int intSpeed = 0;
+            IntPtr ptr;
+            ptr = Marshal.AllocCoTaskMem(4);
+            SystemParametersInfo(SPI_GETMOUSESPEED, 0, ptr, 0);
+            intSpeed = Marshal.ReadInt32(ptr);
+            Marshal.FreeCoTaskMem(ptr);
+
+            return intSpeed;
+        }
+
+        public static void InternalSetMouseSpeed(int intSpeed)
+        {
+            IntPtr ptr = new IntPtr(intSpeed);
+
+            int b = SystemParametersInfo(SPI_SETMOUSESPEED, 0, ptr, 0);
+
+            if (b == 0)
+            {
+                Console.WriteLine("Not able to set speed");
+            }
+            else if (b == 1)
+            {
+                Console.WriteLine("Successfully done");
+            }
+
+        }
+    }
+}
